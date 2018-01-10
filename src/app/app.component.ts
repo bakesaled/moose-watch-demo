0a6932646a70a4f36f862f37c3e9a9e14a484033
Brandon Eleuterio <eleuterio@railroad19.com>
2018-01-10 15:11:32 -0700

    using new strategy for rendering layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutService } from '@bakesaled/moose-watch';
+import { LayoutModel } from '@bakesaled/moose-watch';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public layoutModel: LayoutModel;
   title = 'app';
+
+  constructor(private layoutService: LayoutService) {}
+
+  ngOnInit() {
+    this.layoutService.loadFromFileSystem(
+      new LayoutModel('5c3d0d14-f5d1-4061-92d8-9df65486ee57', 'basic-layout'), './assets/')
+      .subscribe((result) => {
+        localStorage.setItem(result.id, JSON.stringify(result));
+      this.layoutModel = result;
+    });
+  }
 }
